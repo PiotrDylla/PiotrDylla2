26fa4e095eefb4b6d037c0a531b741235e8285be
Piotr Dylla <piotrdylla@gmail.com>
2022-04-15 13:02:32 +0200

    myWork link

--- src/app/myWork/third-container.component.ts
+++ src/app/myWork/third-container.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-third-container',
   templateUrl: './third-container.component.html',
   styleUrls: ['./third-container.component.scss']
 })
 export class ThirdContainerComponent implements OnInit {
+ 
+
+  constructor(public router: Router) { }
 
   goTo(){
+    this.router.navigateByUrl('/ring-of-fire/');
     
   }
-  constructor() { }
+
 
   ngOnInit(): void {
   }
